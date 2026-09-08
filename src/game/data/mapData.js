a60c245fdf6d1bcd42ff3c8b661598f6f81bfc3e
Ryan Amorim <amorim2002ryan@gmail.com>
2026-09-08 10:48:00 -0300

    fix: aumenta espaco interno das salas (v4.12.1)

--- src/game/data/mapData.js
+++ src/game/data/mapData.js
@@ -3,29 +3,33 @@
 
 const NEW_MAP_LAYOUT = [
   '#################################################',
-  '#################...............#################',
-  '#################.....BIANCA....#################',
-  '#################...............#################',
-  '########################D########################',
-  '########################D########################',
-  '#.......##.............................##.......#',
-  '#.......DD........CORREDOR.SUPERIOR....DD.......#',
-  '#NICOLLE##.............................##..RYAN.#',
-  '#.......###############DDD###############.......#',
-  '#.......#####.......................#####.......#',
+  '##############.....................##############',
+  '##############.....................##############',
+  '##############........BIANCA.......##############',
+  '##############.....................##############',
+  '##############.....................##############',
+  '##############.....................##############',
+  '#.......################D################.......#',
+  '#........#.............................#........#',
+  '#........D........CORREDOR.SUPERIOR....D........#',
+  '#NICOLLE.#.............................#...RYAN.#',
+  '#........##############DDD###############.......#',
+  '#........####.......................#####.......#',
+  '#........####.......................#####.......#',
+  '#############....SALÃO.DE.ESPERA....#############',
   '#############.......................#############',
-  '#############....SALAO.DE.ESPERA....#############',
+  '#........####.......................####........#',
+  '#........####.......................####........#',
+  '#........##############DDD##############........#',
+  '#HENRIQUE#.............................#..FELIPE#',
+  '#........D........CORREDOR.INFERIOR....D........#',
+  '#........#.............................#........#',
+  '#........##############DDD##############........#',
   '#############.......................#############',
-  '#.......#####.......................#####.......#',
-  '#.......###############DDD###############.......#',
-  '#HENRIQU##.............................##.FELIPE#',
-  '#.......DD........CORREDOR.INFERIOR....DD.......#',
-  '#.......##.............................##.......#',
-  '#######################DDD#######################',
-  '#################...............#################',
-  'GGGGGGGGG########...............########GGGGGGGGG',
-  'GGGGGGGGG########....RECEPCAO...########GGGGGGGGG',
-  'GGGGGGGGG########.....ENRICO....########GGGGGGGGG',
+  'GGGGGGGGG####.......................####GGGGGGGGG',
+  'GGGGGGGGG####........RECEPÇÃO.......####GGGGGGGGG',
+  'GGGGGGGGG####.........ENRICO........####GGGGGGGGG',
+  'GGGGGGGGG####.......................####GGGGGGGGG',
   'GGGGGGGGG###############################GGGGGGGGG',
 ];
 
@@ -36,14 +40,14 @@ export const TILE_H = 32;
 
 // Retangulos de piso usados para posicionar rotulos dentro das salas.
 export const ROOMS = {
-  entrada: { id: 'entrada', nome: 'Entrada', tipo: 'entrada', rect: { x: 24, y: 19, w: 2, h: 2 } },
-  recepcao: { id: 'recepcao', nome: 'Recepcao Enrico', tipo: 'recepcao', rect: { x: 17, y: 20, w: 15, h: 4 } },
-  sala5: { id: 'sala5', nome: 'Consultorio Bianca', tipo: 'consultorio-grande', rect: { x: 17, y: 1, w: 15, h: 3 } },
-  sala1: { id: 'sala1', nome: 'Consultorio Nicolle', tipo: 'consultorio', rect: { x: 1, y: 6, w: 7, h: 5 } },
-  sala4: { id: 'sala4', nome: 'Consultorio Ryan', tipo: 'consultorio', rect: { x: 41, y: 6, w: 7, h: 5 } },
-  espera: { id: 'espera', nome: 'Salao de Espera', tipo: 'espera', rect: { x: 13, y: 10, w: 23, h: 9 } },
-  sala2: { id: 'sala2', nome: 'Consultorio Henrique', tipo: 'consultorio', rect: { x: 1, y: 14, w: 7, h: 5 } },
-  sala3: { id: 'sala3', nome: 'Consultorio Felipe', tipo: 'consultorio', rect: { x: 41, y: 14, w: 7, h: 5 } },
+  entrada: { id: 'entrada', nome: 'Entrada', tipo: 'entrada', rect: { x: 24, y: 22, w: 2, h: 2 } },
+  recepcao: { id: 'recepcao', nome: 'Recepcao Enrico', tipo: 'recepcao', rect: { x: 13, y: 24, w: 23, h: 4 } },
+  sala5: { id: 'sala5', nome: 'Consultorio Bianca', tipo: 'consultorio-grande', rect: { x: 14, y: 1, w: 21, h: 6 } },
+  sala1: { id: 'sala1', nome: 'Consultorio Nicolle', tipo: 'consultorio', rect: { x: 1, y: 7, w: 8, h: 4 } },
+  sala4: { id: 'sala4', nome: 'Consultorio Ryan', tipo: 'consultorio', rect: { x: 40, y: 7, w: 8, h: 4 } },
+  espera: { id: 'espera', nome: 'Salao de Espera', tipo: 'espera', rect: { x: 13, y: 11, w: 23, h: 8 } },
+  sala2: { id: 'sala2', nome: 'Consultorio Henrique', tipo: 'consultorio', rect: { x: 1, y: 16, w: 8, h: 7 } },
+  sala3: { id: 'sala3', nome: 'Consultorio Felipe', tipo: 'consultorio', rect: { x: 40, y: 16, w: 8, h: 7 } },
 };
 
 function sanitizarLinha(linha) {
@@ -75,13 +79,13 @@ export function gridParaIso(x, y) {
   };
 }
 
-export const POSICAO_INICIAL = { x: 24, y: 21 };
+export const POSICAO_INICIAL = { x: 24, y: 22 };
 
 export const NPCS = [
-  { id: 'enrico', salaId: 'recepcao', nome: 'Enrico', tema: 'recepcao', cor: '#7fb3d5', spriteKey: 'npc-enrico', pos: { x: 24, y: 22 } },
-  { id: 'nicolle', salaId: 'sala1', nome: 'Nicolle', tema: 'Skincare & Fundamentos', cor: '#7fb3d5', pos: { x: 4, y: 8 } },
-  { id: 'henrique', salaId: 'sala2', nome: 'Henrique', tema: 'Toxina Botulínica', cor: '#82c99a', pos: { x: 4, y: 16 } },
-  { id: 'felipe', salaId: 'sala3', nome: 'Felipe', tema: 'Bioestimuladores de Colágeno', cor: '#e0a96d', pos: { x: 44, y: 16 } },
-  { id: 'ryan', salaId: 'sala4', nome: 'Ryan', tema: 'Preenchimentos & Riscos Vasculares', cor: '#d98080', pos: { x: 44, y: 8 } },
-  { id: 'dra_bianca', salaId: 'sala5', nome: 'Dra. Bianca Cirilo', tema: 'Ácido Hialurônico Avançado', cor: '#c5a059', pos: { x: 24, y: 2 } },
+  { id: 'enrico', salaId: 'recepcao', nome: 'Enrico', tema: 'recepcao', cor: '#7fb3d5', spriteKey: 'npc-enrico', pos: { x: 24, y: 26 } },
+  { id: 'nicolle', salaId: 'sala1', nome: 'Nicolle', tema: 'Skincare & Fundamentos', cor: '#7fb3d5', pos: { x: 4, y: 9 } },
+  { id: 'henrique', salaId: 'sala2', nome: 'Henrique', tema: 'Toxina Botulínica', cor: '#82c99a', pos: { x: 4, y: 19 } },
+  { id: 'felipe', salaId: 'sala3', nome: 'Felipe', tema: 'Bioestimuladores de Colágeno', cor: '#e0a96d', pos: { x: 44, y: 19 } },
+  { id: 'ryan', salaId: 'sala4', nome: 'Ryan', tema: 'Preenchimentos & Riscos Vasculares', cor: '#d98080', pos: { x: 44, y: 9 } },
+  { id: 'dra_bianca', salaId: 'sala5', nome: 'Dra. Bianca Cirilo', tema: 'Ácido Hialurônico Avançado', cor: '#c5a059', pos: { x: 24, y: 4 } },
 ];
